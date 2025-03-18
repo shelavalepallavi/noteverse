77629fea716c6fa49d17a830ee3511a8493a0198
shelavale3795 <pallavishelavale3795@gmail.com>
2025-03-18 14:04:40 +0530

    Make change in code

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,9 @@ app.use(cors())
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
+app.get('/', (req, res) => {
+  res.send('Hello!')
+})
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening on port ${port}`)
